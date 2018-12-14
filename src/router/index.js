61a4e199da27c6a914b3ec955d018de7b46f8dcb
chenzhen <1035267741@qq.com>
2018-12-14 11:23:57 +0800

    update

--- src/router/index.js
+++ src/router/index.js
@@ -2,17 +2,13 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Index from '@/components/Index'
 import Contact from '@/components/contact/Contact'
-import Chengshixx from '@/components/duomeiti/Chengshixx'
-import Shuziyy from '@/components/duomeiti/Shuziyy'
-import Yingshihd from '@/components/duomeiti/Yingshihd'
-import Huddmt from '@/components/duomeiti/Huddmt'
-import Show from '@/components/duomeiti/Show'
 import Duomeitsp from '@/components/shejiksh/Duomeitsp'
 import Sanweidh from '@/components/shejiksh/Sanweidh'
 import Vr360 from '@/components/shejiksh/Vr360'
 import Xiaoguotu from '@/components/shejiksh/Xiaoguotu'
 import About from './about.js'
 import Chuangxin from './chuangxin.js'
+import Duomeiti from './duomeiti.js'
 Vue.use(Router)
 const router = new Router({
   mode: 'history',
@@ -24,6 +20,7 @@ const router = new Router({
     },
     ...About,
     ...Chuangxin,
+    ...Duomeiti,
     {
       path: '/contact/contact.html', //联系我们
       name: 'Contact',
@@ -31,36 +28,7 @@ const router = new Router({
       component: Contact
     },
     
-    {
-      path: '/duomeiti/chengshixx.html', //城市形象设计
-      name: 'Chengshixx',
-      meta: { title: "城市形象设计" },
-      component: Chengshixx
-    },
-    {
-      path: '/duomeiti/shuziyy.html', //数字影院
-      name: 'Shuziyy',
-      meta: { title: "数字影院" },
-      component: Shuziyy
-    },
-    {
-      path: '/duomeiti/yingshihd.html', //影视动画
-      name: 'Yingshihd',
-      meta: { title: "影视动画" },
-      component: Yingshihd
-    },
-    {
-      path: '/duomeiti/huddmt.html', //互动多媒体
-      name: 'Huddmt',
-      meta: { title: "互动多媒体" },
-      component: Huddmt
-    },
-    {
-      path: '/duomeiti/show.html', //大型Show策划
-      name: 'Show',
-      meta: { title: "大型Show策划" },
-      component: Show
-    },
+   
     {
       path: '/shejiksh/xiaoguotu.html', //效果图
       name: 'Xiaoguotu',
