9249e5cf456fd305e18259c57652d7741ddb9cef
yyk <yuyongkun4519@qq.com>
2018-12-14 10:47:24 +0800

    update

---
+++ src/router/chuangxin.js
@@ -0,0 +1,22 @@
+import Xshiyanshi from '@/components/chuangxin/Xshiyanshi'
+import Yunxuanran from '@/components/chuangxin/Yunxuanran'
+import Detail from '@/components/chuangxin/Detail'
+export default [{
+    path: '/chuangxin/xshiyanshi.html', //X实验室
+    name: 'Xshiyanshi',
+    meta: { title: "X实验室" },
+    component: Xshiyanshi
+  },
+  {
+    path: '/chuangxin/yunxuanran.html', //云渲染
+    name: 'Yunxuanran',
+    meta: { title: "云渲染" },
+    component: Yunxuanran
+  },
+  {
+    path: '/xshiyanshi/case/:id', //x实验室详情
+    name: 'Detail',
+    meta: { title: "x实验室" },
+    component: Detail,
+  },
+]
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Index from '@/components/Index'
-import About from '@/components/about/About'
-import Dashib from '@/components/about/Dashib'
-import Yinhexgt from '@/components/about/Yinhexgt'
-import Shehuizr from '@/components/about/Shehuizr'
-import Xcjh from '@/components/about/Xcjh'
-import Detail from '@/components/chuangxin/Detail'
 import Contact from '@/components/contact/Contact'
-import Xshiyanshi from '@/components/chuangxin/Xshiyanshi'
-import Yunxuanran from '@/components/chuangxin/Yunxuanran'
 import Chengshixx from '@/components/duomeiti/Chengshixx'
 import Shuziyy from '@/components/duomeiti/Shuziyy'
 import Yingshihd from '@/components/duomeiti/Yingshihd'
@@ -19,6 +11,8 @@ import Duomeitsp from '@/components/shejiksh/Duomeitsp'
 import Sanweidh from '@/components/shejiksh/Sanweidh'
 import Vr360 from '@/components/shejiksh/Vr360'
 import Xiaoguotu from '@/components/shejiksh/Xiaoguotu'
+import About from './about.js'
+import Chuangxin from './chuangxin.js'
 Vue.use(Router)
 const router = new Router({
   mode: 'history',
@@ -28,62 +22,15 @@ const router = new Router({
       meta: { title: "首页" },
       component: Index
     },
-
-    {
-      path: '/about/about.html', //企业简介
-      name: 'About',
-      meta: { title: "企业简介" },
-      component: About
-    },
-    {
-      path: '/about/dashib.html', //丝路视觉“大师杯”
-      name: 'Dashib',
-      meta: { title: "丝路视觉“大师杯”" },
-      component: Dashib
-    },
-    {
-      path: '/about/yinhexgt.html', //内部沙龙：银河星光厅
-      name: 'Yinhexgt',
-      meta: { title: "内部沙龙：银河星光厅" },
-      component: Yinhexgt
-    },
-    {
-      path: '/about/shehuizr.html', //社会责任
-      name: 'Shehuizr',
-      meta: { title: "社会责任" },
-      component: Shehuizr
-    },
-    {
-      path: '/about/xcjh.html', //内部培训：星辰计划
-      name: 'Xcjh',
-      meta: { title: "内部培训：星辰计划" },
-      component: Xcjh
-    },
-    {
-      path: '/xshiyanshi/case/:id', //x实验室详情
-      name: 'Detail',
-      meta: { title: "x实验室" },
-      component: Detail,
-    },
-
+    ...About,
+    ...Chuangxin,
     {
       path: '/contact/contact.html', //联系我们
       name: 'Contact',
       meta: { title: "联系我们" },
       component: Contact
     },
-    {
-      path: '/chuangxin/xshiyanshi.html', //X实验室
-      name: 'Xshiyanshi',
-      meta: { title: "X实验室" },
-      component: Xshiyanshi
-    },
-    {
-      path: '/chuangxin/yunxuanran.html', //云渲染
-      name: 'Yunxuanran',
-      meta: { title: "云渲染" },
-      component: Yunxuanran
-    },
+    
     {
       path: '/duomeiti/chengshixx.html', //城市形象设计
       name: 'Chengshixx',
